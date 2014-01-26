79a05ff9ffdbe160ed9316fdec32c76ffde57af4
Luis Parravicini <lparravi@gmail.com>
2014-01-26 12:21:39 -0300

    version plugin shows cocos2d version used
    Former-commit-id: 5976f4a3dd5f5c0a2e734006a0d9b169ef9a979f

--- console/cocos2d_version.py
+++ console/cocos2d_version.py
@@ -13,9 +13,10 @@
 
 __docformat__ = 'restructuredtext'
 
-
-# python
+import re
+import os
 import cocos2d
+import inspect
 
 
 #
@@ -31,6 +32,44 @@ class CCPluginVersion(cocos2d.CCPlugin):
     def brief_description():
         return "prints the version of the installed components"
 
+    # This is not the constructor, just an initializator
+    def init(self, options, workingdir):
+        self._src_dir = os.path.normpath(options.src_dir)
+        super(CCPluginVersion, self).init(options, workingdir)
+
+    def _show_versions(self):
+    	with open(os.path.join(self._src_dir, "cocos2dx/cocos2d.cpp"), 'r')  as f:
+    		data = f.read()
+    		match = re.search('cocos2dVersion\(\)\s*{\s*return\s+"([^"]+)"\s*;', data)
+    		if match:
+    			print 'cocos2d %s' % match.group(1)
+    		else:
+    			raise cocos2d.CCPluginError("Couldn't find version info")
+
+    # will be called from the cocos2d.py script
     def run(self, argv):
-        print "version called!"
-        print argv
+        self.parse_args(argv)
+        self._show_versions()
+
+    def parse_args(self, argv):
+        from optparse import OptionParser
+
+        parser = OptionParser("usage: %%prog %s -s src_dir -h -v" % CCPluginVersion.plugin_name())
+        parser.add_option("-s", "--src",
+                          dest="src_dir",
+                          help="project base directory")
+        self._add_common_options(parser)
+
+        (options, args) = parser.parse_args(argv)
+
+        if options.src_dir == None:
+            raise cocos2d.CCPluginError("Please set source folder with \"-s\" or \"-src\", use -h for the usage ")
+        else:
+            if os.path.exists(options.src_dir) == False:
+              raise cocos2d.CCPluginError("Error: dir (%s) doesn't exist..." % (options.src_dir))
+
+
+        workingdir = os.path.dirname(inspect.getfile(inspect.currentframe()))
+
+        self.init(options, workingdir)
+
