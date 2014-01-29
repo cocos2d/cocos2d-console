0464c17000cc73844bdb3549b4595a16955ddab3
Luis Parravicini <lparravi@gmail.com>
2014-01-29 13:15:07 -0300

    duplicated code refactored into cocos2d.Platforms
    Former-commit-id: 9f620538d32fe6e3f32e981f8d1db99303b853de

--- plugins/plugin_compile.py
+++ plugins/plugin_compile.py
@@ -32,27 +32,18 @@ class CCPluginCompile(cocos2d.CCPlugin):
     def brief_description():
         return "compiles a project in debug mode"
 
-    def _build_project_dir(self, project_name, display_name):
-        project_dir = os.path.join(self._src_dir, 'proj.android')
-        found = os.path.isdir(project_dir)
-
-        if not found:
-            cocos2d.Logging.warning("No %s project found at %s" % (display_name, project_dir))
-            return None
-
-        return project_dir
-
     def build_android(self):
-        project_dir = self._build_project_dir('proj.android', 'Android')
+        project_dir = self._platforms.android_path
         if project_dir is None:
             return
+
         cocos2d.Logging.info("building native")
         self._run_cmd("cd \"%s\" && ./build_native.sh" % project_dir)
         cocos2d.Logging.info("building apk")
         self._run_cmd("cd \"%s\" && ant debug" % project_dir)
 
     def build_ios(self):
-        project_dir = self._build_project_dir('proj.ios', 'iOS')
+        project_dir = self._platforms.ios_path
         if project_dir is None:
             return
         #TODO do it
