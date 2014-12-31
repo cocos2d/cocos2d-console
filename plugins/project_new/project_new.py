5d237e0e77a1f7f2cee8b76ff1d251aa9f2b9e36
zhangbin <zhangbin@cocos2d-x.org>
2014-12-31 14:32:48 +0800

    Fix the access denied on windows of cocos new.

--- plugins/project_new/project_new.py
+++ plugins/project_new/project_new.py
@@ -266,8 +266,12 @@ class Templates(object):
         self._template_folders = {}
 
         for templates_dir in self._templates_paths:
-            dirs = [name for name in os.listdir(templates_dir) if os.path.isdir(
-                os.path.join(templates_dir, name))]
+            try:
+                dirs = [name for name in os.listdir(templates_dir) if os.path.isdir(
+                    os.path.join(templates_dir, name))]
+            except Exception:
+                continue
+
             pattern = template_pattern[self._lang]
             for name in dirs:
                 match = re.search(pattern, name)
