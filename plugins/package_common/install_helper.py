bf33586ec78ad0da0ccd2c990b532dc28ad0d39f
dualface <dualface@gmail.com>
2015-01-05 23:59:16 +0800

    complete ios_mac support

--- plugins/package_common/install_helper.py
+++ plugins/package_common/install_helper.py
@@ -7,18 +7,21 @@ import shutil
 import cocos
 import json
 import re
+import shlex
 from pprint import pprint
 
 class InstallHelper(object):
-    IOS_LIB_BEGIN_TAG = '\$\(_COCOS_LIB_IOS_BEGIN\)'
-    IOS_LIB_END_TAG = '\$\(_COCOS_LIB_IOS_END\)'
-    MAC_LIB_BEGIN_TAG = '\$\(_COCOS_LIB_MAC_BEGIN\)'
-    MAC_LIB_END_TAG = '\$\(_COCOS_LIB_MAC_END\)'
+    IOS_HEADER_MATCH_TAG     = '(\$\(_COCOS_HEADER_IOS_BEGIN\))(.+)(\$\(_COCOS_HEADER_IOS_END\))';
+    IOS_LIB_BEGIN_TAG        = '\$\(_COCOS_LIB_IOS_BEGIN\)'
+    IOS_LIB_END_TAG          = '\$\(_COCOS_LIB_IOS_END\)'
+
+    MAC_HEADER_MATCH_TAG     = '(\$\(_COCOS_HEADER_MAC_BEGIN\))(.+)(\$\(_COCOS_HEADER_MAC_END\))';
+    MAC_LIB_BEGIN_TAG        = '\$\(_COCOS_LIB_MAC_BEGIN\)'
+    MAC_LIB_END_TAG          = '\$\(_COCOS_LIB_MAC_END\)'
 
     def __init__(self, project, package_data):
-        self._package_path = project["packages_dir"] + os.sep + package_data["name"]
+        self._package_path = project["packages_dir"] + os.sep + package_data["name"] + '-' + package_data["version"]
         self._install_json_path = self._package_path + os.sep + "install.json"
-        print self._install_json_path
         f = open(self._install_json_path, "rb")
         self._commands = json.load(f)
         self._project = project
@@ -57,8 +60,57 @@ class InstallHelper(object):
     def do_add_header_path_on_mac(self, command):
         self.add_header_path_on_ios_mac(command["source"].encode('UTF-8'), "mac")
 
+    def do_add_header_path_on_win(self, command):
+        print "do_add_header_path_on_win skip"
+
+    def do_add_header_path_on_android(self, command):
+        print "do_add_header_path_on_android skip"
+
     def add_header_path_on_ios_mac(self, source, platform):
-        print source, platform
+        if platform == "ios":
+            tag = self.__class__.IOS_HEADER_MATCH_TAG
+        elif platform == "mac":
+            tag = self.__class__.MAC_HEADER_MATCH_TAG
+        else:
+            raise cocos.CCPluginError("Invalid platform '%s'" % platform)
+
+        workdir, proj_pbx_path, lines = self.load_proj_ios_mac()
+        contents = []
+        tag_found = False
+        for line in lines:
+            match = re.search(tag, line)
+            if match is None:
+                contents.append(line)
+            else:
+                includes = shlex.split(match.group(2))
+                headers = []
+                for include in includes:
+                    include = self.get_ios_mac_path(workdir, include)
+                    headers.append(include)
+
+                headers.append(self.get_ios_mac_path(workdir, source))
+                headers = list(set(headers))
+                start, end = match.span(0)
+                parts = []
+                parts.append(line[:start])
+                parts.append(match.group(1))
+                parts.append(' ')
+                for header in headers:
+                    if header.find(' ') != -1:
+                        header = '"' + header + '"'
+                    parts.append(header)
+                    parts.append(' ')
+                parts.append(match.group(3))
+                parts.append(line[end:])
+                contents.append(''.join(parts))
+                tag_found = True
+
+        if tag_found == False:
+            raise cocos.CCPluginError("Not found header TAG in project for platform '%s'" % platform)
+        else:
+            f = open(proj_pbx_path, "wb")
+            f.writelines(contents)
+            f.close()
 
     def do_add_lib_on_ios(self, command):
         self.add_lib_on_ios_mac(command["source"].encode('UTF-8'), "ios")
@@ -66,9 +118,13 @@ class InstallHelper(object):
     def do_add_lib_on_mac(self, command):
         self.add_lib_on_ios_mac(command["source"].encode('UTF-8'), "mac")
 
-    def add_lib_on_ios_mac(self, source, platform):
-        workdir, proj_pbx_path, lines = self.load_proj_ios_mac()
+    def do_add_lib_on_win(self, command):
+        print "do_add_lib_on_win skip"
 
+    def do_add_lib_on_android(self, command):
+        print "do_add_lib_on_android skip"
+
+    def add_lib_on_ios_mac(self, source, platform):
         if platform == "ios":
             begin_tag = self.__class__.IOS_LIB_BEGIN_TAG
             end_tag = self.__class__.IOS_LIB_END_TAG
@@ -78,8 +134,10 @@ class InstallHelper(object):
         else:
             raise cocos.CCPluginError("Invalid platform '%s'" % platform)
 
+        workdir, proj_pbx_path, lines = self.load_proj_ios_mac()
         contents = []
         lib_begin = False
+        tag_found = False
         libs = []
         for line in lines:
             if lib_begin == False:
@@ -87,13 +145,14 @@ class InstallHelper(object):
                 match = re.search(begin_tag, line)
                 if not match is None:
                     lib_begin = True
+                    tag_found = True
             else:
                 match = re.search(end_tag, line)
                 if match is None:
-                    libs.append(self.get_ios_mac_lib_path(workdir, line))
+                    libs.append(self.get_ios_mac_path(workdir, line))
                 else:
                     # add new lib to libs
-                    libs.append(self.get_ios_mac_lib_path(workdir, source))
+                    libs.append(self.get_ios_mac_path(workdir, source))
                     libs = list(set(libs))
                     for lib in libs:
                         contents.append('\t\t\t\t\t"' + lib + '",\n')
@@ -102,15 +161,18 @@ class InstallHelper(object):
                     lib_begin = False
                     contents.append(line)
 
-        f = open(proj_pbx_path, "wb")
-        f.writelines(contents)
-        f.close()
+        if tag_found == False:
+            raise cocos.CCPluginError("Not found lib TAG in project for platform '%s'" % platform)
+        else:
+            f = open(proj_pbx_path, "wb")
+            f.writelines(contents)
+            f.close()
 
-    def get_ios_mac_lib_path(self, project_path, libfilename):
-        libfilename = libfilename.strip(',"\t\n\r')
-        if not libfilename[:10] == '${SRCROOT}':
-            libfilename = '${SRCROOT}' + os.sep + os.path.relpath(self._project["packages_dir"] + os.sep + libfilename, project_path)
-        return libfilename
+    def get_ios_mac_path(self, project_path, source):
+        source = source.strip(',"\t\n\r')
+        if not source[:10] == '$(SRCROOT)':
+            source = '$(SRCROOT)' + os.sep + os.path.relpath(self._project["packages_dir"] + os.sep + source, project_path)
+        return source
 
     def load_proj_ios_mac(self):
         if not "proj.ios_mac" in self._project:
