a152145835c460819ea7a1521f81655345ad6389
hugo <hugohuang1111@gmail.com>
2015-09-08 16:43:57 +0800

    [A] add sdkbox console

--- plugins/plugin_package/plugin_package.py
+++ plugins/plugin_package/plugin_package.py
@@ -12,6 +12,7 @@
 __docformat__ = 'restructuredtext'
 
 import cocos
+import subprocess
 from MultiLanguage import MultiLanguage
 
 class CCPluginPackage(cocos.CCPlugin):
@@ -24,40 +25,15 @@ class CCPluginPackage(cocos.CCPlugin):
         return MultiLanguage.get_string('PACKAGE_BRIEF')
 
     def parse_args(self, argv):
-        if len(argv) < 1:
-            self.print_help()
-            return None
-
         return {"command": argv[0]}
 
     def run(self, argv, dependencies):
-        args = self.parse_args(argv)
-        if args is None:
-            return
+        cmd ='sdkbox ' + ' '.join(argv)
+        self._run_cmd(cmd)
 
-        command = args["command"]
-
-        if command == "search":
-            from package_search import FrameworkAdd
-            CommandClass = FrameworkAdd
-        elif command == "info":
-            from package_info import PackageInfo
-            CommandClass = PackageInfo
-        elif command == "install":
-            from package_install import PackageInstall
-            CommandClass = PackageInstall
-        elif command == "list":
-            from package_list import PackageList
-            CommandClass = PackageList
-        elif command == "-h":
-            self.print_help()
-            return
-        else:
-            message = MultiLanguage.get_string('PACKAGE_ERROR_INVALID_CMD_FMT', command)
-            raise cocos.CCPluginError(message, cocos.CCPluginError.ERROR_CMD_NOT_FOUND)
-
-        commandObject = CommandClass()
-        commandObject.run(argv[1:])
+    def _run_cmd(self, command, cwd=None):
+        # cocos.CMDRunner.run_cmd(command, False, cwd=cwd)
+        subprocess.call(command, shell=True, cwd=cwd)
 
     def print_help(self):
             print(MultiLanguage.get_string('PACKAGE_HELP'))
