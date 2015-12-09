1cfa32b6434a660d64427c638fb47e3e48d12544
lunhao.guo <lunhao.guo@chukong-inc.com>
2015-12-09 15:52:03 +0800

    fix the Environment variable key for COCOS_X_ROOT

--- plugins/plugin_compile/build_android.py
+++ plugins/plugin_compile/build_android.py
@@ -270,9 +270,9 @@ class AndroidBuilder(object):
 
         module_paths = []
         for cfg_path in self.ndk_module_paths:
-            if cfg_path.find("${ENGINE_ROOT}") >= 0:
+            if cfg_path.find("${COCOS_X_ROOT}") >= 0:
                 cocos_root = cocos.check_environment_variable("COCOS_X_ROOT")
-                module_paths.append(cfg_path.replace("${ENGINE_ROOT}", cocos_root))
+                module_paths.append(cfg_path.replace("${COCOS_X_ROOT}", cocos_root))
             elif cfg_path.find("${COCOS_FRAMEWORKS}") >= 0:
                 cocos_frameworks = cocos.check_environment_variable("COCOS_FRAMEWORKS")
                 module_paths.append(cfg_path.replace("${COCOS_FRAMEWORKS}", cocos_frameworks))
