ced47c59d9551b1e63dbf604428f44b0d3747232
lunhao.guo <lunhao.guo@chukong-inc.com>
2015-12-09 22:00:01 +0800

    adding comments for create scene without csb file

--- plugins/plugin_generate/bin-templates/js-template-runtime/src/app.js
+++ plugins/plugin_generate/bin-templates/js-template-runtime/src/app.js
@@ -15,6 +15,27 @@ var HelloWorldLayer = cc.Layer.extend({
         var mainscene = ccs.load(res.MainScene_json);
         this.addChild(mainscene.node);
 
+        /* you can create scene with following comment code instead of using csb file.
+        /////////////////////////////
+        // 3. add your codes below...
+        // add a label shows "Hello World"
+        // create and initialize a label
+        var helloLabel = new cc.LabelTTF("Hello World", "Arial", 38);
+        // position the label on the center of the screen
+        helloLabel.x = size.width / 2;
+        helloLabel.y = size.height / 2 + 200;
+        // add the label as a child to this layer
+        this.addChild(helloLabel, 5);
+
+        // add "HelloWorld" splash screen"
+        this.sprite = new cc.Sprite(res.HelloWorld_png);
+        this.sprite.attr({
+            x: size.width / 2,
+            y: size.height / 2
+        });
+        this.addChild(this.sprite, 0);
+        */
+
         return true;
     }
 });
