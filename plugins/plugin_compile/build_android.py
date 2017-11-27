e63cfbbc82cabbef536034da138b56b7be2814d5
minggo <ming@cocos2d-x.org>
2017-11-27 18:10:46 +0800

    copy apk file from correct path after using build tools 27.0.1, the output apk file path changed

--- plugins/plugin_compile/build_android.py
+++ plugins/plugin_compile/build_android.py
@@ -583,7 +583,7 @@ class AndroidBuilder(object):
             if project_name is None:
                 # use default project name
                 project_name = 'app'
-            gen_apk_folder = os.path.join(self.app_android_root, 'app/build/outputs/apk')
+            gen_apk_folder = os.path.join(self.app_android_root, 'app/build/outputs/apk', build_mode)
         else:
             assets_dir = os.path.join(self.app_android_root, "assets")
             project_name = self._xml_attr(self.app_android_root, 'build.xml', 'project', 'name')
